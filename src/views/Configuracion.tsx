0794cb796572615cb53b0c46b740e40c7965cee2
mafeAS <101601896+mafeAS@users.noreply.github.com>
2025-01-16 21:54:39 -0500

    diseno

--- src/views/Configuracion.tsx
+++ src/views/Configuracion.tsx
@@ -1,22 +1,33 @@
-//import { FloatingLabel } from "flowbite-react";
+import { FloatingLabel } from "flowbite-react";
 import { SparklesCore } from "../ui/sparkles";
 
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { useState } from "react";
+
 const Configuracion = () => {
+  const [age, setAge] = useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
   return (
     <section className=" overflow-hidden">
       <div className="h-screen flex overflow-hidden">
         <div className="h-screen w-1/2 bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
           <div className="text-center max-w-[40rem]">
             <h1 className="md:text-6xl text-3xl font-bold text-center text-white relative z-20 bg-gradient-to-b from-gray-200 via-white to-gray-400 text-transparent bg-clip-text">
-                CONTROL DE GASTOS
-             </h1>
-              <p className="text-center text-white relative z-20 whitespace-pre-line">
-                Antes de continuar, complete el formulario indicando cuánto gana y
-                con qué frecuencia recibe su ingreso para gestionar mejor sus
-                gastos.
-              </p>
+              CONTROL DE GASTOS
+            </h1>
+            <p className="text-center text-white relative z-20 whitespace-pre-line">
+              Antes de continuar, complete el formulario indicando cuánto gana y
+              con qué frecuencia recibe su ingreso para gestionar mejor sus
+              gastos.
+            </p>
           </div>
-           <br />
+          <br />
           <div className="w-[40rem] h-40 relative">
             {/* Gradients */}
             <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
@@ -40,54 +51,73 @@ const Configuracion = () => {
         </div>
         <div className="flex w-1/2 items-center justify-center min-h-screen px-4 py-12 overflow-hidden sm:px-6 lg:px-20 xl:px-24">
           <div className="w-full max-w-xl mx-auto lg:w-96">
+            <div>
+              <h2 className="mt-6 text-3xl font-extrabold text-gray-600">
+                Formulario
+              </h2>
+            </div>
             <div className="mt-8">
               <div className="mt-6">
                 <form action="#" method="POST" className="space-y-6">
                   <div>
-                    <label className="block text-sm font-medium text-gray-600">
-                      {" "}
-                      Sueldo Base{" "}
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        id="email"
-                        name="email"
-                        type="number"
-                        required
-                        placeholder="Ingresa tu sueldo base"
-                        className="block w-full px-5 py-3 text-base text-gray-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
-                      />
-                    </div>
+                    <FloatingLabel
+                      variant="standard"
+                      label="Ingresa tu sueldo base"
+                      type="number"
+                      min={1}
+                    />
                   </div>
 
                   <div className="space-y-1">
-                    <label className="block text-sm font-medium text-gray-600">
-                      {" "}
-                      Frecuencia De Pago{" "}
-                    </label>
-                    <div className="mt-1">
-                      <select
-                        id="password"
-                        name="password"
-                        required
-                        className="block w-full px-5 py-3 text-base text-gray-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
-                      />
-                    </div>
+                  
+                    <FormControl
+                      variant="standard"
+                      className="w-full"
+                    >
+                      <InputLabel id="demo-simple-select-standard-label">
+                        Frecuencia de pago
+                      </InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select-standard"
+                        value={age}
+                        onChange={handleChange}
+                        label="Age"
+                      >
+                        <MenuItem value="">
+                          <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                    </FormControl>
+                    
                   </div>
 
                   <div className="space-y-1">
-                    <label className="block text-sm font-medium text-gray-600">
-                      {" "}
-                      Moneda De Pago{" "}
-                    </label>
-                    <div className="mt-1">
-                      <select
-                        id="password"
-                        name="password"
-                        required
-                        className="block w-full px-5 py-3 text-base text-gray-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
-                      />
-                    </div>
+                  <FormControl
+                      variant="standard"
+                      className="w-full"
+                    >
+                      <InputLabel id="demo-simple-select-standard-label">
+                        Moneda de pago
+                      </InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select-standard"
+                        value={age}
+                        onChange={handleChange}
+                        label="Age"
+                      >
+                        <MenuItem value="">
+                          <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                    </FormControl>
                   </div>
 
                   <div className="flex items-center justify-between">
